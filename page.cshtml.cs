9057fe29739edb04d21774136b5071cc1c5501e5
Bruno Feist <b22.feist@student.stjohns-hamitlon.school.nz>
2024-06-07 17:04:42 +1200

    git commit -m"testing"

---
+++ page.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Testing.Pages;
+{
+    public class myPageModel : pageModel
+    {
+        public void OnGet();
+        {
+
+        }
+
+        public void onPost();
+        {
+            
+        }
+    }
+}
